a0c6477edcb707aba81ac084d924299ed309d290
viX-shaw <songokurocks551@gmail.com>
2019-08-28 16:30:21 +0530

    wip

--- python/KCF.pyx
+++ python/KCF.pyx
@@ -13,7 +13,7 @@ cdef extern from "opencv2/core/cvstd.hpp" namespace "cv":
 
 cdef extern from "opencv2/tracking.hpp" namespace "cv":
 	cdef cppclass TrackerKCF:
-		auto create()	
+		void create()	
 		
 cdef class kcftracker:
 	cdef KCFTracker *classptr
