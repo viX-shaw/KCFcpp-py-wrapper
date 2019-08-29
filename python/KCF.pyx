028bd33f2318259208da0f35557a803561c81caf
viX-shaw <songokurocks551@gmail.com>
2019-08-29 23:04:53 +0530

    wip

--- python/KCF.pyx
+++ python/KCF.pyx
@@ -12,7 +12,9 @@ cdef extern from "opencv2/core/cvstd.hpp" namespace "cv":
 		Ptr()
 
 cdef extern from "ptrtracker.hpp" namespace "tr":
-	pass
+	cdef cppclass Ptr_Tracker:
+		pass
+
 cdef extern from "opencv2/tracking.hpp" namespace "cv":
 	cdef cppclass Tracker:
 		pass
