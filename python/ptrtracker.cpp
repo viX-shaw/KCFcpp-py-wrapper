fa2037aa6558487a5305fb19ce9222ad21c9f288
viX-shaw <songokurocks551@gmail.com>
2019-08-31 00:08:00 +0530

    wip

--- python/ptrtracker.cpp
+++ python/ptrtracker.cpp
@@ -1,9 +1,8 @@
 #include <opencv2/core/utility.hpp>
 #include <opencv2/core/cvstd.hpp>
-#include <opencv2/tracking.hpp>
 #include <iostream>
 #include <cstring>
 
   // create the tracker
 using namespace cv;
-typedef Ptr PtrTracker;
+typedef Ptr<Tracker> PtrTracker;
