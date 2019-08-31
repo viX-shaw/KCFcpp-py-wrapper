1b6e37268173699cf2768609ab1a4641e7b6e6e6
viX-shaw <songokurocks551@gmail.com>
2019-09-01 00:58:59 +0530

    wip

--- python/ptrtracker.hpp
+++ python/ptrtracker.hpp
@@ -11,7 +11,7 @@ public:
     KCFT();
     virtual ~KCFT();
     virtual bool init(cv::Mat img,const cv::Rect2d& box);
-    virtual bool update(cv::Mat img, cv::Rect2d& box);
+    virtual bool update(cv::Mat img, cv::Rect2d box);
 
     cv::Ptr<cv::Tracker> tracker;
 
