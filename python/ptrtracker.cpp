47f2f03a0a5606357bb147f3b7a2e764ee1f211a
viX-shaw <songokurocks551@gmail.com>
2019-09-05 15:41:28 +0530

    wip

--- python/ptrtracker.cpp
+++ python/ptrtracker.cpp
@@ -15,7 +15,7 @@ bool KCFT::init(cv::Mat img, const cv::Rect2d& box)
   return tracker->init(img, box);
 }
 
-bool KCFT::update(cv::Mat img, cv::Rect2d& box)
+bool KCFT::update(cv::Mat img, cv::Rect2d box)
 {
   cv::Rect2d& b = box;
   return tracker->update(img, b);
